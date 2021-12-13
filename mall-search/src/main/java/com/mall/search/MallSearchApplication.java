3fcc700da732e25228c78701cbef68fe3db4256f
zhourui <zhourui@tuhu.cn>
2021-12-13 19:17:30 +0800

    init

--- mall-search/src/main/java/com/mall/search/MallSearchApplication.java
+++ mall-search/src/main/java/com/mall/search/MallSearchApplication.java
@@ -1,10 +1,10 @@
 package com.mall.search;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@NacosPropertySource(dataId = "example", autoRefreshed = true)
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallSearchApplication {
 
--- mall-web/src/main/java/com/mall/search/MallSearchApplication.java
+++ mall-web/src/main/java/com/mall/search/MallSearchApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @NacosPropertySource(dataId = "example", autoRefreshed = true)
 @SpringBootApplication
-public class MallSearchApplication {
+public class MallWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MallSearchApplication.class, args);
+		SpringApplication.run(MallWebApplication.class, args);
 	}
 
 }
